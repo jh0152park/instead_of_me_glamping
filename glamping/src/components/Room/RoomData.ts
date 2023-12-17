6dab10fc19c074d37671564797a6b7579a8b2a6d
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-17 20:22:09 +0900

    fixed room data path

---
+++ glamping/src/components/Room/RoomData.ts
@@ -0,0 +1,27 @@
+export const datas = [
+    {
+        name: "Room A",
+        description: "기준2인, 최대3인",
+        image: require("../../resource/inside.jpg"),
+    },
+    {
+        name: "Room B",
+        description: "기준2인, 최대3인",
+        image: require("../../resource/inside.jpg"),
+    },
+    {
+        name: "Room C",
+        description: "기준2인, 최대3인",
+        image: require("../../resource/inside.jpg"),
+    },
+    {
+        name: "Room D",
+        description: "기준3인, 최대4인",
+        image: require("../../resource/inside.jpg"),
+    },
+    {
+        name: "Room F",
+        description: "기준4인, 최대6인",
+        image: require("../../resource/inside.jpg"),
+    },
+];
